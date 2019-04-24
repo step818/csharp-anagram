167010d2ae14041f071ec5fab31a41ee10c61b07
Maya Frame and Stephen Trewick <maya@>
2019-04-24 15:39:46 -0700

    more backend framework, Main UI setup

--- Anagram.Tests/ModelTests/WordTests.cs
+++ Anagram.Tests/ModelTests/WordTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Anagram.Models;
 
@@ -7,7 +8,56 @@ namespace Anagram.Tests
   public class WordTest
   {
 
-    //
-    
+    [TestMethod]
+    public void WordConstructor_CreatesInstanceOfWord_Word()
+    {
+      Word newWord = new Word("test");
+      Assert.AreEqual(typeof(Word), newWord.GetType());
+    }
+
+    [TestMethod]
+    public void GetInputWord_ReturnInputWord_String()
+    {
+      //Arrange
+      string inputWord = "bread";
+      Word newWord = new Word(inputWord);
+      //Act
+      string result = newWord.GetInputWord();
+      //Assert
+      Assert.AreEqual(inputWord, result);
+    }
+
+    [TestMethod]
+    public void SetInputWord_SetInputWord_String()
+    {
+      string inputWord = "bread";
+      Word newWord = new Word(inputWord);
+      string inputWordList = "bared";
+      newWord.SetInputWord(inputWordList);
+      string result = newWord.GetInputWord();
+      Assert.AreEqual(inputWordList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_WordList()
+    {
+      List<Word> newList = new List<Word> { };
+      List<Word> result = Word.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void IsAnagram_ReturnsTrue_True()
+    {
+      //Arrange
+      string tempInputWord = "bread";
+      string tempInputWordList = "beard";
+
+      //Act
+      CalculateAnagram myAnagram = new CalculateAnagram(tempInputWordList);
+
+      //Assert
+      Assert.AreEqual(true, myAnagram.IsAnagram(tempInputWord, tempInputWordList));
+    }
   }
 }
--- Anagram/Models/Word.cs
+++ Anagram/Models/Word.cs
@@ -1,9 +1,83 @@
+using System;
+using System.Collections.Generic;
+
+
 namespace Anagram.Models
 {
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Hello, please enter a WORD: ");
+      string inputWord = Console.ReadLine();
+
+      List<string> newList = new List<string> {};
+      List<string> anagramList = new List<string> {};
+
+      Console.WriteLine("Enter another word to see if it is an anagram: ");
+      string inputWordList = Console.Readline();
+
+  
+      Console.WriteLine("Would you like to add another word: (Y/N)");
+      string answer = Console.Readline();
+      if (answer == "Y")
+      {
+        while (answer == "Y")
+        {
+          Console.WriteLine("Enter another word to see if it is an anagram: ");
+          string inputWordList = Console.Readline();
+          newList.Add(inputWordList);
+          Console.WriteLine("Would you like to add another word: (Y/N)");
+          string answer = Console.Readline();
+        }
+      }
+      else if (answer == "N")
+      {
+        Console.WriteLine("The list is done adding your words");
+      }
+
+
+
+    }
+  }
+
+  public class CalculateAnagram
+  {
+    public bool IsAnagram()
+    {
+
+    }
+
+  }
+
+
+
+
+
+
+
+
+
   public class Word
   {
+    private string _inputWord;
 
-    //
-    
+    public Word (string inputWord)
+    {
+      _inputWord = inputWord;
+    }
+    public string GetInputWord()
+    {
+      return _inputWord;
+    }
+
+    public void SetInputWord(string newInputWord)
+    {
+      _inputWord = newInputWord;
+    }
+    public static List<Word> GetAll()
+    {
+      return new List<Word> {};
+    }
   }
 }
